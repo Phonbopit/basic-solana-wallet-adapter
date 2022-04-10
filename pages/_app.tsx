377ff17750db3a8b0e87f6f7e7de74aa12fae97d
Chai Phonbopit <phonbopit@gmail.com>
2022-04-10 13:46:15 +0700

    chore: use import instead of require

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ import {
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 
-require("@solana/wallet-adapter-react-ui/styles.css");
+import "@solana/wallet-adapter-react-ui/styles.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
